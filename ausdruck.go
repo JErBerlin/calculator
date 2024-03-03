77ef199f27da55724018340f9716c9517a66ac06
Jeroni Rodriguez <jeronir@hotmail.com>
2024-03-03 17:15:13 +0100

    add separated parse functions partial parse and base parse and parse number to ausdruck.go

--- ausdruck.go
+++ ausdruck.go
@@ -12,65 +12,158 @@ type Ausdr interface {
 	Wert() (int, error)
 }
 
-// baueAusdr baut einen Ausdruck aus einer Rune-Zeichenkette im Reader
-func baueAusdr(r io.Reader) (Ausdr, error) {
-	reader := bufio.NewReader(r)
+// ParseAusdruck baut einen Ausdruck rekursiv aus einer Zeichenkeete r
+func ParseAusdruck(r *bufio.Reader) (Ausdr, error) {
+	a, err := ParseTeilAusdruck(r, 0)
+	if err != nil {
+		return nil, err
+	}
+	return a, nil
+}
 
-	var aktuelleVerkn Verkn
-	var strZahl StrZahl // String um die gelesenen Ziffer zu speichern und dann zum Nummerischen Wert zu konvertieren
+// ParseTeilausdruck vearbeitet Teil-Ausdruecke basierend auf der Prioritaet der Operationen
+func ParseTeilAusdruck(r *bufio.Reader, minPrio int) (Ausdr, error) {
 
-	// lese eine grupe der Form A | A + B | A - B | A * B | A / B
-	// noch keine Parenthesis erlaubt
+	// Als erstes Parse linke Ausdruck, bis zum Klammer: z.B. (links) + (rechts)
+	links, err := ParseGrundAusdruck(r)
+	if err != nil {
+		return nil, err
+	}
+
+	// rekusive Schleife
+	// von links nach rechts und durch prio
 	for {
-		symbol, _, err := reader.ReadRune()
+		op, err := leseRune(r)
+		if err != nil {
+			if err == io.EOF {
+				return links, nil
+			}
+			return nil, err
+		}
 
+		prio := OpPrio(op)
+		if prio < minPrio { // op gehoert einer höheren Ebene der Rekursion / des Baums
+			r.UnreadRune()    // -> leg das Symbol zurück
+			return links, nil // Ausdruck besteht nur aus dem linken Teil
+		}
+
+		rechts, err := ParseTeilAusdruck(r, prio+1) // rekursives Aufrufen im rechten Ausdruck
+		if err == io.EOF {
+			return links, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		verkn, err := leseVerknupfung(op)
+		if err != nil {
+			return nil, err
+		}
+
+		// Auswertung von links-Ausdr A und rechts-Ausdr B
+		linksWert, err := links.Wert()
+		if err != nil {
+			return nil, err
+		}
+		verkn.SetA(linksWert)
+
+		rechtsWert, err := rechts.Wert()
+		if err == io.EOF {
+			return links, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		verkn.SetB(rechtsWert)
+
+		links = verkn // Setze aktuelle Verknüpfung als neuen linken Operanden
+	}
+
+}
+
+// ParseGrundAusdruck liest und verarbeitet Zahlen und geklammerte Ausdruecke.
+// Praekondition: Ausdr faengt an mit Linksklammer '(', Zahl oder Leerzeichen.
+// Z.B. "1" -> 1 und "(1 + 1)" -> 1 + 1
+// Im Ausdr koenen verschiedene Ebene von Klammer geben: 0( 1( 2( ... ) ) )
+func ParseGrundAusdruck(r *bufio.Reader) (Ausdr, error) {
+	symbol, err := leseRune(r)
+	if err != nil {
+		return nil, err
+	}
+
+	if unicode.IsDigit(symbol) { // parse Zahl
+		r.UnreadRune() // TODO: better way than unread?
+		return ParseZahl(r)
+
+	} else if symbol == '(' { // parse Ausdruck zwischen Klammern
+		a, err := ParseTeilAusdruck(r, 0) // fang an mit Rekursionsebene 0
+		if err != nil {
+			return nil, err
+		}
+
+		wantRune := ')' // erwarte schließende Klammer
+		gotRune, err := leseRune(r)
+		if err != nil || gotRune != wantRune {
+			return nil, fmt.Errorf("beim Parsen Aussenausdruk: erwartet ')', gefunden %q", gotRune)
+		}
+
+		return a, nil
+	} else {
+		return nil, fmt.Errorf("unerwartetes Symbol %q", symbol)
+	}
+}
+
+// ParseZahl liest und verarbeitet eine Zahl aus der Zeichenkette.
+func ParseZahl(r *bufio.Reader) (Ausdr, error) {
+	var strZahl StrZahl
+	for {
+		symbol, err := leseRune(r)
 		if err == io.EOF {
 			break // Dateiende
 		}
-		// sonstiger Fehler, stop
-		if err != nil {
-			return nil, fmt.Errorf("Fehler beim Lesen: %v", err)
-		}
 
-		// sortiere Leerzeichen aus
+		if !unicode.IsDigit(symbol) {
+			r.UnreadRune() // Keine Ziffer mehr, Symbol zurücklegen
+			break
+		}
+		strZahl.Hinzu(symbol)
+	}
+
+	if strZahl.Leer() {
+		return nil, fmt.Errorf("Fehler beim Parsen einer Zahl")
+	}
+
+	w, err := strZahl.Wert()
+	if err != nil {
+		return nil, err
+	}
+
+	return Ganzezahl{w}, nil
+}
+
+// leseRune liest die naechste Rune aus der Zeichenkette und ignoriert die Leerzeichen.
+func leseRune(r *bufio.Reader) (rune, error) {
+	for {
+		symbol, _, err := r.ReadRune()
+		if err != nil {
+			return 0, err
+		}
 		if unicode.IsSpace(symbol) {
 			continue
 		}
 
-		// lese eine Ziffer von Operanden a oder Operanden b in die Stringzahl
-		if unicode.IsDigit(symbol) {
-			strZahl.Hinzu(symbol)
-			continue
-		}
-
-		// kein leerzeichen und keine Ziffer: versuche, eine Verknupfung zu lesen
-		if aktuelleVerkn, err = leseVerknupfung(symbol); err != nil {
-			return nil, fmt.Errorf("leseVerkn: Fehler beim Lesen der Verknüpfung: %s", err)
-		}
-
-		// speichere Operanden A in die Verknupfung
-		zahl, err := strZahl.Wert()
-		if err != nil {
-			return nil, fmt.Errorf("baueAusdr: Fehler beim Konvertieren Zeichen zu Zahl für Operanden")
-		}
-		aktuelleVerkn.SetA(zahl)
-
-		// bereit für Operanden B
-		strZahl.Reset()
+		return symbol, nil
 	}
+}
 
-	// Die Zeichenkette eintält eine Zahl alleine oder den zweiten Operanden B
-	letzteZahl, err := strZahl.Wert()
-	if err != nil {
-		// etwas ist schiefgelaufen: wir brauchen wenigstens eine Zahl
-		return nil, fmt.Errorf("die Zeichenkette enthält keine Zahl: %s", err)
+// OpPrio bestimmt die Prioritaet der Operationen
+func OpPrio(op rune) int {
+	switch op {
+	case '*', '/':
+		return 2
+	case '+', '-':
+		return 1
+	default:
+		return 0
 	}
-
-	// der Ausdrueck ist entweder eine Zahl oder eine Verknupfung
-	if aktuelleVerkn == nil {
-		return Ganzezahl{letzteZahl}, nil
-	}
-
-	aktuelleVerkn.SetB(letzteZahl)
-	return aktuelleVerkn, nil
 }
